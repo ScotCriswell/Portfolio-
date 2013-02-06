6e331a2bb89242c78f2e7f7ca01ea488f48bda51
Scot Criswell <scotcriswell@gmail.com>
2013-02-05 21:42:04 -0600

    Version 1Portfolio

---
+++ contactthanks.php
@@ -0,0 +1,83 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+
+
+	<title>Thanks!</title>
+	<!-- ================================================== Basic Page Needs ================================================== -->
+		<meta charset="utf-8" />
+		<title>Scot Criswell :: Clean, clear and creative web and print design from Springfield, Missouri.</title>
+		<meta name="description" content="Scot Criswell, Design, Designer, Freelance, Portfolio, Web Design, Print Design, Logo, branding, Identity, business design, business cards, packaging design, Grahpic designer, Grahpic designer for hire, Springfield, Springfield Missouri,Grahpic designer for hire in Springfield Missouri">
+		<meta name="author" content="Scot Criswell">
+		
+	<!-- ================================================== Mobile Specific Metas ================================================== -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+		
+	<!-- ================================================== CSS ================================================== -->
+		<link rel="stylesheet" href="stylesheets/base.css">
+		<link rel="stylesheet" href="stylesheets/skeleton.css">
+		<link rel="stylesheet" href="stylesheets/layout.css">
+		<link href="webfonts/ss-social.css" rel="stylesheet" />
+		<link href='http://fonts.googleapis.com/css?family=Droid+Serif' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Glegoo' rel='stylesheet' type='text/css'>
+		
+	<!-- ==================================================  Favicons  ================================================== -->
+	    <link rel="shortcut icon" href="images/favicon.ico">
+	    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+	    
+	<!-- ================================================== javscript ================================================== -->
+	<script type="text/javascript" src="//use.typekit.net/itm3trq.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	   
+	
+</head>
+
+<body>
+
+     <div id="wrapper" class="shadow"> 
+        <div id="header" class="container">
+          <div class="head">
+     	   <div id="logo" ><img src= "images/logo.png" alt="Scot Criswell Designer"/>
+     	     </div>
+     		<nav id="respnav" class="fixed">
+     		<ul class="nav">
+     			<li class="nav"><a href="index.html">X CLOSE</a></li>
+     		</ul>
+     		</nav>	
+     	  </div>
+        </div>	
+     </div>
+     
+
+
+
+     
+	 <div class="container thumb" id="awesome"> 
+	    <img src="images/awesome.png">
+	 </div>
+	 	 	
+	<!-- fade in page load 	-->	
+	     <script>
+	          jQuery(document).ready(function(){
+	          delay();
+	           });
+	           
+	           function delay() {
+	              var secs = 200;
+	              setTimeout('initFadeIn()', secs);
+	           }
+	     
+	          function initFadeIn() {
+	              jQuery("body").css("visibility","visible");
+	              jQuery("body").css("display","none");
+	              jQuery("body").fadeIn(300);
+	          }
+	     </script>
+
+</body>
+
+</html>
